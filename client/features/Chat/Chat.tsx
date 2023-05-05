40e866e287344cf136894fdd811bb4796da736cf
Yury Borets <udborets@gmail.com>
2023-05-05 16:27:16 +0300

    add chat component

---
+++ client/features/Chat/Chat.tsx
@@ -0,0 +1,14 @@
+import { FC } from "react"
+import MessageInput from "./MessageInput/MessageInput"
+import Messages from "./Messages/Messages"
+
+const Chat: FC = () => {
+  return (
+    <div className="w-full h-full flex flex-col justify-end">
+      <Messages />
+      <MessageInput />
+    </div>
+  )
+}
+
+export default Chat
