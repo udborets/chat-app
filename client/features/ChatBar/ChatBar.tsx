94c8f1e7a138ef0ac6ed21405561ed38aa81105c
Yury Borets <udborets@gmail.com>
2023-05-05 15:51:03 +0300

    change chat bar max width

--- client/features/ChatBar/ChatBar.tsx
+++ client/features/ChatBar/ChatBar.tsx
@@ -6,7 +6,7 @@ import styles from './styles.module.scss';
 
 const ChatBar: FC = () => {
   return (
-    <div className="flex flex-col gap-4 bg-slate-500 h-full max-w-[300px] w-full p-2">
+    <div className="flex flex-col gap-4 bg-slate-500 h-full max-w-[400px] w-full p-2">
       <SearchBar />
       <ul className={`flex flex-col gap-2 overflow-y-scroll ${styles.ChatScrollBar}`}>
         <ChatItem />
