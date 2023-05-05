f97a788644e3b4608e565985531d29ef9f35d335
Yury Borets <udborets@gmail.com>
2023-05-05 21:07:36 +0300

    add text chats

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -3,11 +3,47 @@ import { FC } from "react";
 import PageLayout from "@/layouts/PageLayout/PageLayout";
 import ChatBar from "@/features/ChatBar/ChatBar";
 import Chat from "@/features/Chat/Chat";
+import { ChatBarProps } from "@/features/ChatBar/models";
+
+const testChats: ChatBarProps = {
+  chats: [
+    {
+      companionAvatar: '',
+      companionName: "test 1",
+      id: Math.random(),
+      lastMessage: "Hello!",
+    },
+    {
+      companionAvatar: '',
+      companionName: "test 1",
+      id: Math.random(),
+      lastMessage: "Hello!",
+    },
+    {
+      companionAvatar: '',
+      companionName: "test 1",
+      id: Math.random(),
+      lastMessage: "Hello!",
+    },
+    {
+      companionAvatar: '',
+      companionName: "test 1",
+      id: Math.random(),
+      lastMessage: "Hello!",
+    },
+    {
+      companionAvatar: '',
+      companionName: "test 1",
+      id: Math.random(),
+      lastMessage: "Hello!",
+    },
+  ]
+}
 
 const HomePage: FC = () => {
   return (
     <PageLayout title="YouChat" className="w-full h-full flex">
-      <ChatBar />
+      <ChatBar {...testChats} />
       <Chat />
     </PageLayout>
   )
