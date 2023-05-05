e9ef21f0a070c1197981036bff45c06464e65519
Yury Borets <udborets@gmail.com>
2023-05-05 21:07:20 +0300

    add avatar image and fix styles in chat item

--- client/features/ChatBar/ChatItem/ChatItem.tsx
+++ client/features/ChatBar/ChatItem/ChatItem.tsx
@@ -1,15 +1,24 @@
+import Image from 'next/image';
+import { FC } from 'react';
+
+import noAvatarWhite from './assets/no-avatar-white.png';
+import { ChatItemProps } from './models';
 import styles from './styles.module.scss';
 
-const ChatItem = () => {
+const ChatItem: FC<ChatItemProps> = ({ companionAvatar, companionName, id, lastMessage }) => {
   return (
-    <li className={`${styles.bgAnimation} flex w-[96%] h-[100px] py-1 px-3 justify-center items-center gap-4 rounded-[10px]`}>
-      <div className="rounded-[50%] h-[45px] min-h-[45px] w-[45px] min-w-[45px] bg-white" />
+    <li className={`${styles.bgAnimation} flex w-[96%] h-[70px] min-h-[70px] text-[1.2rem] py-1 px-3 justify-center items-center gap-4 rounded-[10px]`}>
+      <Image
+        src={noAvatarWhite}
+        alt='avatar'
+        className="rounded-[50%] h-[45px] min-h-[45px] w-[45px] min-w-[45px] "
+      />
       <div className="h-full flex w-full flex-col justify-around ">
-        <span className="text-[1.1rem] font-bold">
-          Sender 1
+        <span className="font-bold">
+          {companionName}
         </span>
         <span>
-          Text 1
+          {lastMessage}
         </span>
       </div>
     </li>
