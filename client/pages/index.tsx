bb26432b172c0c0873802a9e11da157bc4621971
Yury Borets <udborets@gmail.com>
2023-05-05 16:27:25 +0300

    add chat component to home page

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -2,11 +2,13 @@ import { FC } from "react";
 
 import PageLayout from "@/layouts/PageLayout/PageLayout";
 import ChatBar from "@/features/ChatBar/ChatBar";
+import Chat from "@/features/Chat/Chat";
 
 const HomePage: FC = () => {
   return (
-    <PageLayout title="YouChat" className="w-full h-full">
+    <PageLayout title="YouChat" className="w-full h-full flex">
       <ChatBar />
+      <Chat />
     </PageLayout>
   )
 }
