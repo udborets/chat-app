8cfe3b8450b04b8cd51682ac9d8c7a382eeae5c6
Yury Borets <udborets@gmail.com>
2023-05-05 19:53:28 +0300

    fix chat item styles

--- client/features/ChatBar/ChatItem/ChatItem.tsx
+++ client/features/ChatBar/ChatItem/ChatItem.tsx
@@ -1,8 +1,10 @@
+import styles from './styles.module.scss';
+
 const ChatItem = () => {
   return (
-    <li className="flex w-[96%] h-[70px] bg-black py-1 px-3 justify-center items-center gap-4 rounded-[10px]">
+    <li className={`${styles.bgAnimation} flex w-[96%] h-[100px] py-1 px-3 justify-center items-center gap-4 rounded-[10px]`}>
       <div className="rounded-[50%] h-[45px] min-h-[45px] w-[45px] min-w-[45px] bg-white" />
-      <div className="h-full text-white flex w-full flex-col justify-around ">
+      <div className="h-full flex w-full flex-col justify-around ">
         <span className="text-[1.1rem] font-bold">
           Sender 1
         </span>
