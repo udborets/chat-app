d52f3decf161b6c2957560cc2e3add3ef357aec3
Yury Borets <udborets@gmail.com>
2023-05-05 16:26:42 +0300

    add message input component

---
+++ client/features/Chat/MessageInput/MessageInput.tsx
@@ -0,0 +1,15 @@
+import { FC, useRef } from "react"
+
+const MessageInput: FC = () => {
+  const messageInputRef = useRef<HTMLInputElement>(null);
+  return (
+    <input
+      className="w-full py-2 px-3 bg-gray-300 text-[1.2rem]"
+      placeholder="Start typing a message..."
+      ref={messageInputRef}
+      type="text"
+    />
+  )
+}
+
+export default MessageInput
