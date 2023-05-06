82088701c463cba44f736899baed2872894dd508
Yury Borets <udborets@gmail.com>
2023-05-06 20:17:02 +0300

    add top bar content

--- client/features/Chat/TopBar/TopBar.tsx
+++ client/features/Chat/TopBar/TopBar.tsx
@@ -1,7 +1,23 @@
-const TopBar = () => {
-  return (
-    <div>TopBar</div>
-  )
-}
+import { observer } from "mobx-react-lite";
+import Image from "next/image";
 
-export default TopBar
+import { chatBarState } from "@/store/ChatBarState";
+import menuIconWhite from './assets/menuIconWhite.png';
+
+const TopBar = observer(() => {
+  return (
+    <div className="w-full h-[60px] min-h-[40px] flex border-b-2 px-4 items-center">
+      <button
+        onClick={() => chatBarState.toggleIsActive()}
+        className="h-fit w-fit p-3">
+        <Image
+          className="w-[20px] h-[20px]"
+          src={menuIconWhite}
+          alt='menu icon'
+        />
+      </button>
+    </div>
+  )
+})
+
+export default TopBar;
