f76d527d9e14782a0d0fc32e94621bbd193bf39e
Yury Borets <udborets@gmail.com>
2023-05-06 10:54:59 +0300

    change chat item avatar image scale

--- client/features/ChatBar/ChatItem/ChatItem.tsx
+++ client/features/ChatBar/ChatItem/ChatItem.tsx
@@ -15,7 +15,7 @@ const ChatItem: FC<ChatItemProps> = ({ companionAvatar, companionName, id, lastM
       <Image
         src={noAvatarWhite}
         alt='avatar'
-        className="rounded-[50%] h-[45px] min-h-[45px] w-[45px] min-w-[45px] "
+        className="rounded-[50%] h-[50px] min-h-[50px] w-[50px] min-w-[50px] "
       />
       <div className="h-full flex w-full flex-col justify-around ">
         <span className="font-bold">
