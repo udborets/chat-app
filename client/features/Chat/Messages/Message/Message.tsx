bd903c7f4f8d5b9523049d4dd9b29078c480dbaf
Yury Borets <udborets@gmail.com>
2023-05-06 19:33:24 +0300

    fix message scale

--- client/features/Chat/Messages/Message/Message.tsx
+++ client/features/Chat/Messages/Message/Message.tsx
@@ -5,19 +5,19 @@ import styles from './styles.module.scss';
 
 const Message: FC<MessageProps> = ({ id, isOwn, text, sendingTime, isRead }) => {
   return (
-    <div className={`w-full min-h-[40px] flex ${isOwn ? "justify-end" : "justify-start"}`}>
-      <div className={`flex items-end ${isOwn ? 'mr-4' : 'ml-4'}`}>
+    <div className={`w-full h-fit flex ${isOwn ? "justify-end" : "justify-start"}`}>
+      <div className={`flex items-end w-fit h-fit ${isOwn ? 'mr-4' : 'ml-4'}`}>
         {!isOwn
           ? <span className={styles.triangle}></span>
           : ''}
-        <span
+        <p
           className={`${isOwn
             ? 'color-bg rounded-[10px] rounded-br-[0]'
             : 'message-bg rounded-[10px] rounded-bl-[0]'
             } w-fit h-fit min-h-[40px] min-w-[70px] max-w-[300px] p-2 text-left outline-none border-none`}
         >
           {text}
-        </span>
+        </p>
         {isOwn
           ? <span className={styles.ownTriangle}></span>
           : ''}
