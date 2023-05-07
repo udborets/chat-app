68bd83abfeb4fc5192e0dfa467d3462768c7e697
Yury Borets <udborets@gmail.com>
2023-05-07 08:54:34 +0300

    add message input content

--- client/features/Chat/MessageInput/MessageInput.tsx
+++ client/features/Chat/MessageInput/MessageInput.tsx
@@ -1,14 +1,36 @@
-import { FC, useRef } from "react";
+import Image from "next/image";
+import { FC, useRef, useState } from "react";
+
+import sendIconWhite from './assets/sendIconWhite.png';
 
 const MessageInput: FC = () => {
   const messageInputRef = useRef<HTMLInputElement>(null);
+  const [messageText, setMessageText] = useState<string>('');
   return (
-    <input
-      className="w-full py-2 px-3 message-bg text-main text-[1.2rem]"
-      placeholder="Start typing a message..."
-      ref={messageInputRef}
-      type="text"
-    />
+    <div className="w-full h-fit flex justify-center items-center absolute bottom-[10px] left-0 gap-2 pc:gap-4">
+      <input
+        className="w-[80%] py-2 px-3 chats-bg text-main text-[1rem] rounded-[50px]"
+        placeholder="Start typing a message..."
+        ref={messageInputRef}
+        type="text"
+        value={messageText}
+        onChange={(e) => setMessageText(e.target.value)}
+      />
+      <button className={`${messageText !== ''
+        ? 'color-bg hover:color-bg-hover active:color-bg-active'
+        : 'message-bg'} rounded-[50%] duration-200 transition-all w-fit h-fit p-2`}>
+        <Image
+          onClick={() => {
+            if (messageText !== '') {
+              console.log('haha')
+            }
+          }}
+          className="w-[20px] h-[20px] "
+          src={sendIconWhite}
+          alt='send icon'
+        />
+      </button>
+    </div>
   )
 }
 
