22989a8fbc1293a8fe57585383e15943471899e8
Yury Borets <udborets@gmail.com>
2023-05-13 21:02:50 +0300

    add menu component

---
+++ client/features/Menu/Menu.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react";
+
+const Menu: FC = () => {
+  return (
+    <aside
+      className="max-w-4/5 pc:max-w-[400px] w-full "
+    >
+
+    </aside>
+  )
+}
+
+export default Menu;
