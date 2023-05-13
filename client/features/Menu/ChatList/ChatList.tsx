ca07fc85891cc49b650752c212efebc7cdbb8494
Yury Borets <udborets@gmail.com>
2023-05-13 21:02:31 +0300

    add chat list component

---
+++ client/features/Menu/ChatList/ChatList.tsx
@@ -0,0 +1,14 @@
+import { FC } from "react"
+import { ChatListProps } from "./models"
+
+const ChatList: FC<ChatListProps> = ({ }) => {
+  return (
+    <ul
+      className=""
+    >
+
+    </ul>
+  )
+}
+
+export default ChatList
