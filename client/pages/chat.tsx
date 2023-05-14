2ca8d6a51f88616c058bf366afe670234edf5812
Yury Borets <udborets@gmail.com>
2023-05-14 09:31:48 +0300

    add chat page

---
+++ client/pages/chat.tsx
@@ -0,0 +1,14 @@
+import ChatBody from "@/features/ChatBody/ChatBody";
+import { FC } from "react";
+
+const ChatPage: FC = () => {
+  return (
+    <main
+      className="w-full h-full"
+    >
+      <ChatBody />
+    </main>
+  )
+}
+
+export default ChatPage;
