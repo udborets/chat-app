b74d09b506de23a4174e74447f03409d38fe98a9
Yury Borets <udborets@gmail.com>
2023-05-15 07:46:55 +0300

    add redux store provider to app root

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,13 +1,15 @@
 import type { AppProps } from 'next/app';
 import { FC } from 'react';
+import { Provider } from 'react-redux';
 
+import { store } from '@/store';
 import '@/styles/global.scss';
 
 const App: FC<AppProps> = ({ Component, pageProps }) => {
   return (
-    <>
+    <Provider store={store}>
       <Component {...pageProps} />
-    </>
+    </Provider>
   )
 }
 
