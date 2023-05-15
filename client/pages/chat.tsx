91e30c136d04e8dbf95a8cc4fde89573dd2edbd6
Yury Borets <udborets@gmail.com>
2023-05-15 14:12:23 +0300

    add chat menu to chat page

--- client/pages/chat.tsx
+++ client/pages/chat.tsx
@@ -1,11 +1,13 @@
 import ChatBody from "@/features/ChatBody/ChatBody";
+import ChatMenu from "@/features/ChatMenu/ChatMenu";
 import { FC } from "react";
 
 const ChatPage: FC = () => {
   return (
     <main
-      className="w-full h-full"
+      className="w-full h-full flex"
     >
+      <ChatMenu />
       <ChatBody />
     </main>
   )
