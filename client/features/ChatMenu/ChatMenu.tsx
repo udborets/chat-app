49b4ae9d36d6c9c45e52de79fdc3c9f6f0c429ba
Yury Borets <udborets@gmail.com>
2023-05-15 14:12:15 +0300

    add test code

--- client/features/ChatMenu/ChatMenu.tsx
+++ client/features/ChatMenu/ChatMenu.tsx
@@ -1,11 +1,12 @@
 import { FC } from "react";
+import ChatList from "./ChatList/ChatList";
 
 const ChatMenu: FC = () => {
   return (
     <aside
-      className="max-w-4/5 pc:max-w-[400px] w-full "
+      className="max-w-4/5 pc:max-w-[270px] w-full "
     >
-
+      <ChatList />
     </aside>
   )
 }
