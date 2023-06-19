1ed7e0e16893d418315d748ff6dbc65ca1eed337
IskanderSh <is2367361@gmail.com>
2023-06-19 18:48:54 +0300

    add core and mocks for testing auth

---
+++ server/internal/handlers/auth_test.go
@@ -0,0 +1,15 @@
+package handlers
+
+import "testing"
+
+func TestAuth_UserSignUp(t *testing.T) {
+
+}
+
+func TestAuth_UserSignIn(t *testing.T) {
+
+}
+
+func TestAuth_RequireAuth(t *testing.T) {
+
+}
